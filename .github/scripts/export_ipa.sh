02710c8149493b0dbe80b241fe661ca6ae0fc69f
levelgitpersonal <144824214+levelgitpersonal@users.noreply.github.com>
2024-08-23 20:02:08 +0300

    build 1.0

---
+++ .github/scripts/export_ipa.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eo pipefail
+
+xcodebuild -archivePath build/"$NAME".xcarchive \
+            -exportOptionsPlist .github/exportOptions.plist \
+            -exportPath $PWD/build \
+            -allowProvisioningUpdates \
+            -exportArchive | xcpretty
